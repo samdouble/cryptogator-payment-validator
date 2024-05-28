7b1cce34e5ef3df53fade7245af72f2c0bf4b9d6
samdouble <samleafs@protonmail.com>
2024-05-27 21:06:22 -0400

    feat: call Etherscan API to query transactions and logs on an address

--- environment.d.ts
+++ environment.d.ts
@@ -1,6 +1,8 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      ADDRESS_USDC: string;
+      ADDRESS_USDT: string;
       ETHERSCAN_API_SERVER: string;
       ETHERSCAN_API_TOKEN: string;
       NODE_ENV: 'development' | 'production';
--- src/endpoints.ts
+++ src/endpoints.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import http from 'http';
 import https from 'https';
+import contractAddresses from './contractAddresses';
 
 const ApiClient = axios.create({
-  headers: {
-    Authorization: `Bearer ${process.env.ETHERSCAN_API_TOKEN}`,
-  },
   httpAgent: new http.Agent({ keepAlive: true }),
   httpsAgent: new https.Agent({ keepAlive: true }),
   maxRedirects: 10,
@@ -14,13 +12,52 @@ const ApiClient = axios.create({
   withCredentials: true,
 });
 
-export const fetchOneEvent = async (filter = {}) => {
-  const pFilter = filter && `filter=${JSON.stringify(filter)}`;
+export const fetchErc20TransactionForAddress = async (erc20Token, address, filter = {}) => {
+  const filterWithCredentials = {
+    ...filter,
+    apikey: process.env.ETHERSCAN_API_TOKEN,
+    module: 'account',
+    action: 'tokentx',
+    contractaddress: contractAddresses[erc20Token],
+    address,
+    // page:
+    // offset:
+    // startblock:
+    // endblock:
+    sort: 'asc',
+  };
+
+  const pFilter = filterWithCredentials && `filter=${JSON.stringify({filterWithCredentials})}`;
   const query = [pFilter]
     .filter(q => !!q)
     .join('&');
+
   return ApiClient
-    .get(`${process.env.ETHERSCAN_API_SERVER}/v1/events${query && `?${query}`}`)
-    .then(res => res.data.events[0])
+    .get(`${process.env.ETHERSCAN_API_SERVER}/api${query && `?${query}`}`)
+    .then((res: any) => res.result)
+    .catch(error => console.error(`Fetch events error ${error}`));
+}
+
+export const fetchLogsForAddress = async (address, filter = {}) => {
+  const filterWithCredentials = {
+    ...filter,
+    apikey: process.env.ETHERSCAN_API_TOKEN,
+    module: 'logs',
+    action: 'getLogs',
+    address,
+    // fromBlock
+    // toBlock
+    // page:
+    // offset:,
+  };
+
+  const pFilter = filterWithCredentials && `filter=${JSON.stringify({filterWithCredentials})}`;
+  const query = [pFilter]
+    .filter(q => !!q)
+    .join('&');
+
+  return ApiClient
+    .get(`${process.env.ETHERSCAN_API_SERVER}/api${query && `?${query}`}`)
+    .then((res: any) => res.result)
     .catch(error => console.error(`Fetch events error ${error}`));
 }
