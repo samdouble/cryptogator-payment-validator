b5971c8c2aeb9ef583a5ae99445cf115f93a2126
samdouble <samleafs@protonmail.com>
2024-05-27 21:06:22 -0400

    feat: call Etherscan API to query transactions and logs on an address

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,19 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
+import { fetchErc20TransactionForAddress, fetchLogsForAddress } from './endpoints';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+  const [transactionsUsdc, transactionsUsdt, logsUsdc, logsUsdt] = await Promise.all([
+    fetchErc20TransactionForAddress('usdc', process.env.ADDRESS_USDC),
+    fetchErc20TransactionForAddress('usdt', process.env.ADDRESS_USDT),
+    fetchLogsForAddress(process.env.ADDRESS_USDC),
+    fetchLogsForAddress(process.env.ADDRESS_USDT),
+  ]);
+
+  console.log('Transactions USDC', transactionsUsdc);
+  console.log('Transactions USDT', transactionsUsdt);
+  console.log('Logs USDC', logsUsdc);
+  console.log('Logs USDT', logsUsdt);
+
   return {
     statusCode: 200,
     body: JSON.stringify({
