55091b7d0755c174a30940ecaa78e24643c77d06
samdouble <samleafs@protonmail.com>
2024-06-24 16:38:10 -0400

    feat: record SOL transactions

--- src/transactions/schemas/SolanafmTransaction.ts
+++ src/transactions/schemas/SolanafmTransaction.ts
@@ -1,30 +1,14 @@
 import { Schema } from 'mongoose';
 
 export interface ISolanafmTransaction {
-  blockHash: string;
-  blockNumber: number;
-  cumulativeGasUsed: number;
-  gas: number;
-  gasPrice: number;
-  gasUsed: number;
-  hash: string;
-  nbConfirmations: number;
-  nonce: number;
-  transactionIndex: number;
+  receiverAssociation?: string;
+  senderAssociation: string;
 }
 
 export const SolanafmTransactionSchema: Schema = new Schema<ISolanafmTransaction>(
   {
-    blockHash: { type: String, required: true },
-    blockNumber: { type: Number, required: true },
-    cumulativeGasUsed: { type: Number, required: true },
-    gas: { type: Number, required: true },
-    gasPrice: { type: Number, required: true },
-    gasUsed: { type: Number, required: true },
-    hash: { type: String, required: true },
-    nbConfirmations: { type: Number, required: true },
-    nonce: { type: Number, required: true },
-    transactionIndex: { type: Number, required: true },
+    receiverAssociation: { type: String, required: true },
+    senderAssociation: { type: String, required: true },
   },
 );
 
