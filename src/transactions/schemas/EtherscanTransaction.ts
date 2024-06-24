b55bdc8a549289ccbf3f2dc591fb58e6af13dad6
samdouble <samleafs@protonmail.com>
2024-06-24 16:38:10 -0400

    feat: record SOL transactions

--- src/transactions/schemas/EtherscanTransaction.ts
+++ src/transactions/schemas/EtherscanTransaction.ts
@@ -4,10 +4,8 @@ export interface IEtherscanTransaction {
   blockHash: string;
   blockNumber: number;
   cumulativeGasUsed: number;
-  gas: number;
   gasPrice: number;
   gasUsed: number;
-  hash: string;
   nbConfirmations: number;
   nonce: number;
   transactionIndex: number;
@@ -18,10 +16,8 @@ export const EtherscanTransactionSchema: Schema = new Schema<IEtherscanTransacti
     blockHash: { type: String, required: true },
     blockNumber: { type: Number, required: true },
     cumulativeGasUsed: { type: Number, required: true },
-    gas: { type: Number, required: true },
     gasPrice: { type: Number, required: true },
     gasUsed: { type: Number, required: true },
-    hash: { type: String, required: true },
     nbConfirmations: { type: Number, required: true },
     nonce: { type: Number, required: true },
     transactionIndex: { type: Number, required: true },
